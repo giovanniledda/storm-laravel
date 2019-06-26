e79eeadc5ad67c59ee8ab3ef0b48d0b7a7462d3d
Giovanni Ledda <ledda@netseven.it>
2019-06-26 18:48:58 +0200

    Aggiunto titolo al Task

--- app/Task.php
+++ app/Task.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Venturecraft\Revisionable\RevisionableTrait;
 
 class Task extends Model
 {
+    use RevisionableTrait;
+
     public function project()
     {
         return $this->morphOne('App\Project', 'projectable');
