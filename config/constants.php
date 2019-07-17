113c5ed2abb5c8675f5c69c29051d959f3027ba6
Antonio Reina <reina@netseven.it>
2019-07-17 16:55:20 +0200

    sistemata la definizione delle costanti

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,5 @@
 <?php
-
-define('CLASS_NAME_BOAT', 'App/Boat');
-define('SECTION_NAME_LEFT_SIDE', 'left_side');
-define('SECTION_NAME_RIGHT_SIDE', 'right_side');
-define('SECTION_NAME_DECK', 'deck');
+defined('CLASS_NAME_BOAT') or define('CLASS_NAME_BOAT', 'App/Boat');
+defined('SECTION_NAME_LEFT_SIDE') or define('SECTION_NAME_LEFT_SIDE', 'left_side');
+defined('SECTION_NAME_RIGHT_SIDE') or define('SECTION_NAME_RIGHT_SIDE',  'right_side');
+defined('SECTION_NAME_DECK') or define('SECTION_NAME_DECK', 'deck');
