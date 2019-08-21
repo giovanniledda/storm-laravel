df971d32db87c801196f494ba2752301e7c74184
Giovanni Ledda <ledda@netseven.it>
2019-08-21 09:34:21 +0200

    New packages: web-tinker e telescope-bar

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home'; // previously '/home';
+    protected $redirectTo = '/dashboard'; // previously '/home';
 
     /**
      * Create a new controller instance.
