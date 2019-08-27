f7ae38f58f67cd0907d5cbbda784e62f8e72deb8
danilo <giacomi@netseven.it>
2019-08-27 17:12:40 +0200

    routes

--- packages/Net7/Documents/src/routes/routes.php
+++ packages/Net7/Documents/src/routes/routes.php
@@ -5,10 +5,11 @@ Route::group(['middleware' => ['auth:api', 'logoutBlocked']], function () {
 
     JsonApi::register('v1', ['namespace'=>'Api'])->routes(function ($api) {
 
-        $api->resource('documents')->only('create', 'show', 'show_with_size')->controller('\Net7\Documents\DocumentsController') //uses the App\Http\Controllers\Api\DocumentController
+        $api->resource('documents')->only('show')->controller('\Net7\Documents\DocumentsController') //uses the App\Http\Controllers\Api\DocumentController
         ->routes(function ($docs){
                 $docs->get('{record}/show/{size}', 'show')->name('show_with_size');
                 $docs->get('{record}/show', 'show')->name('show');
+                $docs->post('create', 'create')->name('create');
 
                 // if (config('net7documents.use_collection'))  // aggiungere la variabile al file di conf
                 // per attivarla aggiungere 'index' nell'only poco sopra
