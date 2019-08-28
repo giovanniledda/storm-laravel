5be3ac3be233be94d04228f74bdc57458cee930a
Giovanni Ledda <ledda@netseven.it>
2019-08-28 17:11:54 +0200

    refact documents

--- packages/Net7/Documents/src/baseJsonApiClasses/Documents/Schema.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\JsonApi\V1\Documents;
-
-use \Net7\Documents\JsonApi\V1\Documents\Schema as BaseSchema;
-
-class Schema extends BaseSchema
-{
-}
--- packages/Net7/Documents/src/config/net7documents.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-
-    'json_api' => true,
-    'json_api_namespace' => 'app/JsonApi/V1/Documents'
-];
