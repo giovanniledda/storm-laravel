16b89153d8e37fa9298c3444a02a1983727d3246
Antonio Reina <reina@netseven.it>
2019-10-02 12:13:11 +0200

    Aggiunta la data inizio e fine di progetto nello schema della risorsa projects

--- app/JsonApi/V1/Projects/Schema.php
+++ app/JsonApi/V1/Projects/Schema.php
@@ -85,6 +85,8 @@ class Schema extends SchemaProvider
             'imported'  => $resource->imported,
             'project_progress'=> $resource->project_progress,
             'project_type' => $resource->project_type,
+            'start_date' => $resource->start_date->toAtomString(),
+            'end_date' => $resource->end_date->toAtomString(),
             'created-at' => $resource->created_at->toAtomString(),
             'updated-at' => $resource->updated_at->toAtomString(),
         ];
