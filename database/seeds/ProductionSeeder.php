01de3807d0a52fbfa780eb8bcd3821effeead3ec
danilo <giacomi@netseven.it>
2019-10-14 17:12:50 +0200

    refresh_prod

--- database/seeds/ProductionSeeder.php
+++ database/seeds/ProductionSeeder.php
@@ -5,16 +5,21 @@ use App\Site;
 use App\Profession;
 use App\TaskInterventType;
 //use Faker\Factory as Faker;
+use Seeds\SeederUtils as Utils;
+
 
 class ProductionSeeder extends Seeder
 {
 //    protected $faker;
-
+protected $utils;
+protected $professions;
     public function run()
     {
+
+        $this->utils = new Utils();
         $this->populateSites();
 
-        $this->populateProfessions();
+        $this->professions = $this->populateProfessions();
 
         $this->populateTaskTypes();
     }
@@ -25,19 +30,42 @@ class ProductionSeeder extends Seeder
 
         $this->command->warn("\n\n ________________ Creating Sites ________________\n\n");
 
-        $sites = \Config::get('storm.startup.sites');
-        foreach ($sites as $site => $fields) {
 
-            $s = Site::create(Arr::except($fields, ['addresses']));
-            $this->command->info("Site {$s->name} [OK]");
+        $site = $this->utils->createSite();
 
-            $addresses = $fields['addresses'];
-            foreach ($addresses as $addr => $addr_fields) {
+        $site->setName('Cantieri Benetti Livorno')
+            ->setLocation('Via Edda Fagni, 1, 57100 – Livorno, Italy')
+            ->setLat(43.5430829)
+            ->setLng(10.2994663)
+            ->save();
 
-                $s->addAddress($addr_fields);
-                $this->command->info("Address $addr for Site {$s->name} [OK]");
-            }
-        }
+        $this->command->info("Site {$site->name} created");
+
+
+        $site2 = $this->utils->createSite();
+
+        $site2->setName('NCA Refit')
+            ->setLocation('Viale Colombo, 4Bis 54033 – Marina di Carrara (MS) Italy')
+            ->setLat(43.9763331)
+            ->setLng(10.0188368)
+            ->save();
+
+        $this->command->info("Site {$site2->name} created");
+
+
+        // $sites = \Config::get('storm.startup.sites');
+        // foreach ($sites as $site => $fields) {
+
+        //     $s = Site::create(Arr::except($fields, ['addresses']));
+        //     $this->command->info("Site {$s->name} [OK]");
+
+        //     $addresses = $fields['addresses'];
+        //     foreach ($addresses as $addr => $addr_fields) {
+
+        //         $s->addAddress($addr_fields);
+        //         $this->command->info("Address $addr for Site {$s->name} [OK]");
+        //     }
+        // }
     }
 
     private function populateProfessions()
@@ -45,11 +73,21 @@ class ProductionSeeder extends Seeder
 
         $this->command->warn("\n\n ________________ Creating Professions ________________\n\n");
 
-        $professions = \Config::get('storm.startup.professions');
-        foreach ($professions as $profession => $fields) {
-            $p = Profession::create($fields);
-            $this->command->info("Profession {$p->name} [OK]");
+
+        $professions = [];
+        $professions[0] = $this->utils->createProfession('owner');
+        for ($s = 1; $s < 20; $s++) {
+            $professions[$s] = $this->utils->createProfession('worker');
+
+            $this->command->info("Profession {$professions[$s]->name} created");
         }
+
+        // $professions = \Config::get('storm.startup.professions');
+        // foreach ($professions as $profession => $fields) {
+        //     $p = Profession::create($fields);
+        //     $this->command->info("Profession {$p->name} [OK]");
+        // }
+        return $professions;
     }
 
     private function populateTaskTypes()
@@ -61,6 +99,13 @@ class ProductionSeeder extends Seeder
         foreach ($intervent_types as $task_type => $fields) {
             $tit = TaskInterventType::create($fields);
             $this->command->info("Task Intervent Type {$tit->name} [OK]");
+        }
+
+    }
+
+    private function createUsers(){
+        for ($s = 0; $s < 25; $s++) {
+            $worker = $this->utils->createUser(ROLE_WORKER);
         }
 
     }
