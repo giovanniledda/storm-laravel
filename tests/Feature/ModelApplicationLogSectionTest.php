0012fa287e9671c23410f9432faa833fa18a0762
Giovanni Ledda <ledda@netseven.it>
2019-12-11 17:11:35 +0100

    refact

--- tests/Feature/ModelApplicationLogSectionTest.php
+++ tests/Feature/ModelApplicationLogSectionTest.php
@@ -14,8 +14,10 @@ class ModelApplicationLogSectionTest extends TestCase
      */
     public function testBasicRelationships()
     {
+        $application_log_sections_num = $this->faker->numberBetween(1, 15);
+
         $application_log = factory(ApplicationLog::class)->create();
-        $application_log_sections = factory(ApplicationLogSection::class, 5)->create();
+        $application_log_sections = factory(ApplicationLogSection::class, $application_log_sections_num)->create();
 
         /** application_log **/
         /** $table->foreign('application_log_id')->references('id')->on('application_logs')->onDelete('set null'); **/
@@ -23,7 +25,7 @@ class ModelApplicationLogSectionTest extends TestCase
         // assegno le app log sections (5 elementi) all'app log
         $application_log->application_log_sections()->saveMany($application_log_sections);
 
-        $this->assertEquals(5, $application_log->application_log_sections()->count());
+        $this->assertEquals($application_log_sections_num, $application_log->application_log_sections()->count());
 
         foreach ($application_log_sections as $application_log_section) {
             $this->assertEquals($application_log_section->application_log_id, $application_log->id);
