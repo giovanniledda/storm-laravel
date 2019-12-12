1d64f7f2ce624b61e26876036f8c8d4cd33bdfab
Giovanni Ledda <ledda@netseven.it>
2019-12-12 15:28:21 +0100

    stesrua jsonapi per zonification

---
+++ app/JsonApi/V1/Zones/Adapter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\JsonApi\V1\Zones;
+
+use CloudCreativity\LaravelJsonApi\Eloquent\AbstractAdapter;
+use CloudCreativity\LaravelJsonApi\Pagination\StandardStrategy;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+
+class Adapter extends AbstractAdapter
+{
+
+    /**
+     * Mapping of JSON API attribute field names to model keys.
+     *
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * Mapping of JSON API filter names to model scopes.
+     *
+     * @var array
+     */
+    protected $filterScopes = [];
+
+    /**
+     * Adapter constructor.
+     *
+     * @param StandardStrategy $paging
+     */
+    public function __construct(StandardStrategy $paging)
+    {
+        parent::__construct(new \App\Zone(), $paging);
+    }
+
+    /**
+     * @param Builder $query
+     * @param Collection $filters
+     * @return void
+     */
+    protected function filter($query, Collection $filters)
+    {
+        $this->filterWithScopes($query, $filters);
+    }
+
+
+    // Relationships (https://laravel-json-api.readthedocs.io/en/latest/basics/adapters/)
+
+    protected function parent_zone()
+    {
+        return $this->belongsTo('parent_zone_id');
+    }
+
+    public function children_zones()
+    {
+        return $this->hasMany('parent_zone_id');
+    }
+}
---
+++ app/JsonApi/V1/Zones/Schema.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\JsonApi\V1\Zones;
+
+use App\Task;
+use App\Zone;
+use Neomerx\JsonApi\Schema\SchemaProvider;
+
+class Schema extends SchemaProvider
+{
+
+    /**
+     * @var string
+     */
+    protected $resourceType = 'zones';
+
+    /**
+     * @var array
+     */
+    protected $relationships = [
+        'parent_zone',
+        'children_zones',
+    ];
+
+    /**
+     * @param $resource
+     *      the domain record being serialized.
+     * @return string
+     */
+    public function getId($resource)
+    {
+        return (string) $resource->getRouteKey();
+    }
+
+    /**
+     * @param $resource
+     *      the domain record being serialized.
+     * @return array
+     */
+    public function getAttributes($resource)
+    {
+        return [
+            // campi
+            'code' => $resource->code,
+            'description' => $resource->description,
+            'extension' => $resource->extension,
+            'created-at' => $resource->created_at->toAtomString(),
+            'updated-at' => $resource->updated_at->toAtomString(),
+            // relazioni
+            'parent_zone' => $resource->parent_zone,  // parent_zone - zones
+            'children_zones' => $resource->children_zones,  // children_zones - zones
+        ];
+    }
+
+    /**
+     * @param Zone $zone
+     * @param bool $isPrimary
+     * @param array $includedRelationships
+     * @return array
+     */
+    public function getRelationships($zone, $isPrimary, array $includedRelationships)
+    {
+        return [
+            'parent_zone' => [
+                self::SHOW_SELF => true,
+                self::SHOW_RELATED => true,
+                self::SHOW_DATA => isset($includedRelationships['parent_zone']),
+                self::DATA => function () use ($zone) {
+                    return $zone->parent_zone;
+                },
+            ],
+            'children_zones' => [
+                self::SHOW_SELF => true,
+                self::SHOW_RELATED => true,
+                self::SHOW_DATA => isset($includedRelationships['children_zones']),
+                self::DATA => function () use ($zone) {
+                    return $zone->children_zones;
+                },
+            ]
+        ];
+    }
+}
