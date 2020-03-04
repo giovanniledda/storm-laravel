d143e1291f773ac49114a2bd3b4a4178c6144a29
Luca Landi <luca.landi@isiadesign.fi.it>
2020-03-04 18:03:38 +0100

    Prova sei Corrosion Map

--- app/Task.php
+++ app/Task.php
@@ -753,15 +753,16 @@ EOF;
         // creo la tabella a seconda delle immagini che ho
         if (!empty($photos_array) && count($photos_array) > 1) {
             $tds_1 = <<<EOF
-                    <td width="300">
+                    <td width="750">
                       <img height="200"  src="file://$photos_array[1]" alt="Corrosion img 1">
+                      <img height="200"  src="file://$photos_array[2]" alt="Corrosion img 2">
+                      <img height="200" src="file://$photos_array[3]" alt="Corrosion img 3">
+                      <img height="200"  src="file://$photos_array[4]" alt="Corrosion img 4">
                     </td>
 EOF;
             if (isset($photos_array[2])) {
                 $tds_1 .= <<<EOF
-                    <td width="300">
-                      <img height="200"  src="file://$photos_array[2]" alt="Corrosion img 2">
-                    </td>
+                    
 EOF;
             }
 
@@ -769,23 +770,19 @@ EOF;
 
             if (isset($photos_array[3])) {
                 $tds_2 = <<<EOF
-                    <td width="300">
-                      <img height="200" src="file://$photos_array[3]" alt="Corrosion img 3">
-                    </td>
+                    
 EOF;
 
                 if (isset($photos_array[4])) {
                     $tds_2 .= <<<EOF
-                        <td width="300">
-                          <img height="200"  src="file://$photos_array[4]" alt="Corrosion img 4">
-                        </td>
+                       
 EOF;
                 }
 
                 $trs .= '<tr>' . $tds_2 . '</tr>';
             }
 
-            $images_table = '<p style="text-align: left;font-size: 16px;font-weight: bold; font-family: Raleway, sans-serif;">Detail photos</p>
+            $images_table = '<br><p style="text-align: left;font-size: 16px;font-weight: bold; font-family: Raleway, sans-serif;">Detail photos</p>
                                <table><tbody>' . $trs . '<tr height="50"></tr></tbody></table>';
 
             $html .= $images_table;
