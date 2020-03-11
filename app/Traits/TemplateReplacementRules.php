caa68c9e17bb4206570d37cdcd925dec72f60f9b
Giovanni Ledda <ledda@netseven.it>
2020-03-11 15:38:00 +0100

    soglia per eliminare righe verticali

--- app/Traits/TemplateReplacementRules.php
+++ app/Traits/TemplateReplacementRules.php
@@ -375,8 +375,9 @@ trait TemplateReplacementRules
                 $max_scale = $env_param->getMaximum();
                 $min_scale = $min_threshold ? min($min_threshold, $env_param->getMinimum()) : $env_param->getMinimum();
             }
+            $measurements_num = count($measurements);
 
-            throw_if(!count($measurements), new \Exception("No data in this date range, for this data source!", 403));
+            throw_if(!$measurements_num, new \Exception("No data in this date range, for this data source!", 403));
 
             /** @var Measurement $measurement */
             foreach ($measurements as $measurement) {
@@ -404,7 +405,7 @@ trait TemplateReplacementRules
                 'haxLabelDisplay' => 'vertical',
                 'vaxLabelDisplay' => 0,
                 'hgrid' => '1',
-                'vgrid' => '1',
+                'vgrid' => $measurements_num > 100 ? 0 : '1',
                 'scalingMax' => $max_scale,
                 'scalingMin' => $min_scale,
                 'horizontalOffset' => 360,
